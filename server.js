a89c1cd579d728e15fe30b17bd1e52412249969d
Naomi Thian <nc090290@gmail.com>
2025-03-13 03:02:22 -0400

    [Naomi] Update Order Online page with image section and remove footer; Update server.js

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.get('/orderOnline', (req, res) => {
 });
 
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
